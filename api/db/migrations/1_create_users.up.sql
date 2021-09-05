4837ca8f891577950c9d0df5a40ff8320617ac28
Shunya Endo <shunyaendoh.bz@gmail.com>
2021-09-05 16:36:59 +0900

    👍 chore(token): change genarating token logic

--- api/db/migrations/1_create_users.up.sql
+++ api/db/migrations/1_create_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `users` (
   `id` varchar(26) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `token` varchar(255) NOT NULL,
+  `token` varchar(500) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime,
